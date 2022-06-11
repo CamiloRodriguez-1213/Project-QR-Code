33cd2e117606eec14b8cf862740e59eb15b03c29
Camilo Rodriguez <andrescamilocr482@gmail.com>
2022-06-11 01:00:59 -0500

    Quitada opcion ver

--- models/deleteUrlUserModel.py
+++ models/deleteUrlUserModel.py
@@ -2,7 +2,7 @@ from config.database import db
 cursor = db.cursor()
 def deleteUrl(id):
     try:
-        cursor.execute("DELETE FROM shortener where id_shortener = '"+id+"'")
+        cursor.execute("DELETE FROM shortener where id_shortener = "+id+"")
         db.commit()
         return True
     except:
